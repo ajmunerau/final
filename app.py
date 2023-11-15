1e1c6572a0c28d6c59ccfa0b634e5c007ef7a5ee
ajmunerau <60147060+ajmunerau@users.noreply.github.com>
2023-11-15 18:21:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,38 +3,32 @@ import cv2
 import numpy as np
 from datetime import datetime
 
-# Función para mostrar la hora actual
 def display_clock():
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     st.sidebar.markdown(f'#### ⏰ Hora Actual: {current_time}')
 
-# Función para determinar si el color está en el rango especificado
 def is_color_in_range(image, lower_color, upper_color):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_color, upper_color)
     coverage = np.sum(mask > 0) / mask.size
     return coverage > 0.2
 
-# Cargar clasificador preentrenado de OpenCV
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 st.title("Sistema de Control de Acceso")
-display_clock()  # Mostrar el reloj
+display_clock() 
 
-# Ruta al archivo de la imagen de acceso denegado
-denied_image_path = "ruta/a/la/imagen/WhatsApp Image 2023-11-15 at 6.09.37 PM.jpeg"
+denied_image_path = "LB.jpeg"
 
 uploaded_file = st.sidebar.file_uploader("Carga una imagen", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     img = cv2.imdecode(file_bytes, 1)
 
-    # Detectar rostros
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    # Definir rangos de color HSV para negro
     black_lower = np.array([0, 0, 0], np.uint8)
     black_upper = np.array([180, 255, 30], np.uint8)
 
@@ -57,7 +51,6 @@ if uploaded_file is not None:
                     """, unsafe_allow_html=True)
                 break  # Si se detecta la persona conocida, no es necesario seguir buscando
 
-        # Si no se reconoce a la persona con camiseta negra, mostrar la imagen y los formularios
         if not known_person_detected:
             st.image(img, channels="BGR", use_column_width=True)
             if num_faces == 1:
@@ -67,7 +60,6 @@ if uploaded_file is not None:
                 st.subheader("Acceso Permitido")
                 st.write(f"{num_faces} personas detectadas.")
 
-            # Crear formularios para cada rostro detectado
             for i in range(num_faces):
                 with st.form(key=f'Form{i}'):
                     st.subheader(f'Información del Invitado {i+1}')
@@ -79,8 +71,6 @@ if uploaded_file is not None:
                     if submitted:
                         st.success('Invitados registrados')
     else:
-       
- # Si no se detectan rostros, mostrar imagen de acceso denegado
         st.subheader("Acceso bloqueado")
         st.write("No se detectaron rostros")
         st.image(denied_image_path, caption='Acceso Denegado', use_column_width=True)
