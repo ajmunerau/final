92633d28c8c5f49461922ad337333951ef976721
ajmunerau <60147060+ajmunerau@users.noreply.github.com>
2023-11-15 16:38:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,24 @@
+import streamlit as st
 import cv2
-
-# Inicializar la cámara
-cap = cv2.VideoCapture(0)
+import numpy as np
 
 # Cargar el clasificador preentrenado de rostros de OpenCV
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-while True:
-    # Capturar imagen por imagen
-    ret, frame = cap.read()
+st.title("Detector de Rostros")
 
-    # Convertir a escala de grises para la detección de rostros
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+uploaded_file = st.file_uploader("Carga una imagen", type=["jpg", "jpeg", "png"])
+if uploaded_file is not None:
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, 1)
 
     # Detectar rostros
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
     # Dibujar rectángulos alrededor de los rostros
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
     # Mostrar el resultado
-    cv2.imshow('Control de Acceso - Detección de Rostros', frame)
-
-    # Salir con 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Liberar la cámara y cerrar todas las ventanas
-cap.release()
-cv2.destroyAllWindows()
+    st.image(img, channels="BGR", use_column_width=True)
