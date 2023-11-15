de338c7318326c197c384a20a7186bce4cea9099
ajmunerau <60147060+ajmunerau@users.noreply.github.com>
2023-11-15 17:20:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,31 +25,29 @@ if uploaded_file is not None:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    # Definir rangos de color para cabello amarillo y camiseta negra en HSV
-    yellow_lower = np.array([20, 100, 100], np.uint8)
-    yellow_upper = np.array([30, 255, 255], np.uint8)
+    # Definir rangos de color para camiseta negra en HSV
     black_lower = np.array([0, 0, 0], np.uint8)
     black_upper = np.array([180, 255, 30], np.uint8)
 
     num_faces = len(faces)
     if num_faces > 0:
         for (x, y, w, h) in faces:
-            # Análisis de color de cabello y camiseta
-            hair_region = img[max(y - h//3, 0):y, x:x+w]
+            # Análisis de color de camiseta
             shirt_region = img[y+h:y+h+h//2, x:x+w]
-            hair_yellow = is_color_in_range(hair_region, yellow_lower, yellow_upper)
             shirt_black = is_color_in_range(shirt_region, black_lower, black_upper)
 
             # Dibujar rectángulo alrededor del rostro
             cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-            # Mostrar información si se detecta cabello amarillo y camiseta negra
-            if hair_yellow and shirt_black:
-                cv2.putText(img, "Cabello Amarillo, Camiseta Negra", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            # Mostrar información si se detecta camiseta negra
+            if shirt_black:
+                cv2.putText(img, "Camiseta Negra Detectada", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         
         if num_faces == 1:
             st.subheader("¡Puedes entrar a la casa!")
             st.write("Eres una persona")
+            if shirt_black:
+                st.write("Andrés Julián Múnera Uribe\n23 años\nCédula 1001011725\nEstudiante de Diseño Interactivo")
         else:
             st.subheader("¡Pueden entrar a la casa!")
             st.write(f"Se detectaron {num_faces} rostros")
@@ -59,7 +57,3 @@ if uploaded_file is not None:
 
     # Mostrar el resultado
     st.image(img, channels="BGR", use_column_width=True)
-
-
-
-
