23e8257cf544e86fba66d7efb6eae7a7b6118f41
ajmunerau <60147060+ajmunerau@users.noreply.github.com>
2023-11-15 16:59:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 # Cargar el clasificador preentrenado de rostros de OpenCV
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-st.title("Detector de Rostros")
+st.title("Sistema de Control de Acceso")
 
 uploaded_file = st.file_uploader("Carga una imagen", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
@@ -16,10 +16,17 @@ if uploaded_file is not None:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-    # Dibujar rectángulos alrededor de los rostros
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    # Dibujar rectángulos alrededor de los rostros y mostrar mensajes según la detección
+    if len(faces) > 0:
+        st.subheader("¡Puedes entrar a la casa!")
+        st.write("Eres una persona")
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    else:
+        st.subheader("Acceso bloqueado")
+        st.write("No se detectaron rostros")
 
     # Mostrar el resultado
     st.image(img, channels="BGR", use_column_width=True)
+
 
